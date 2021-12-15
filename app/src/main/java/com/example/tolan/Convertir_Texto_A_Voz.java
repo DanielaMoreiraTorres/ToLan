b9652739002d497600fd76be2f7fc2bbd7520a45
DanielaMoreiraTorres <mishellmoreira_1997@hotmail.com>
2021-12-14 22:48:21 -0500

    ACTIVITY WELCOME AND CLASS TEXTTOSOUND

---
+++ app/src/main/java/com/example/tolan/Convertir_Texto_A_Voz.java
@@ -0,0 +1,39 @@
+package com.example.tolan;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+
+import java.util.Locale;
+
+public class Convertir_Texto_A_Voz {
+
+    private static TextToSpeech tts = null;
+    TextToSpeech.OnInitListener onInitListener;
+
+    public void init(Context context){
+        try {
+            tts = new TextToSpeech(context, onInitListener = new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int i) {
+                    if(i != TextToSpeech.ERROR){
+                        tts.setLanguage(Locale.getDefault());
+                    }
+                }
+            });
+        }catch (Exception e){
+            Log.e("TTS", "Initilization Failed!");
+        }
+    }
+
+    public void reproduce(String texto) {
+            tts.speak(texto,TextToSpeech.QUEUE_FLUSH,null);
+    }
+
+    public void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tolan/Welcome.java
@@ -0,0 +1,48 @@
+package com.example.tolan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+
+import org.imaginativeworld.whynotimagecarousel.CarouselItem;
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Welcome extends AppCompatActivity {
+    private List list;
+    ImageCarousel carousel;
+    static TextToSpeech textToSpeech;
+    Convertir_Texto_A_Voz tts;
+    Button btnIniciar;
+    Button btnRegistrarse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        getSupportActionBar().hide();
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i!= TextToSpeech.ERROR){
+                    textToSpeech.setLanguage(Locale.getDefault());
+                    textToSpeech.speak(getString(R.string.bienvenida),TextToSpeech.QUEUE_FLUSH,null);
+                }
+            }
+        });
+        tts = new Convertir_Texto_A_Voz();
+        tts.init(this);
+        carousel = findViewById(R.id.carousel);
+        list = new ArrayList();
+        list.add(new CarouselItem(R.drawable.aprendizaje,getString(R.string.mensaje_bienvenida_1)));
+        list.add(new CarouselItem(R.drawable.nino,getString(R.string.mensaje_bienvenida_2)));
+        list.add(new CarouselItem(R.drawable.ninos,getString(R.string.mensaje_bienvenida_3)));
+        carousel.addData(list);
+    }
+}
