f0474a2511d8ceb1c8b2757031b664a4ab3acac0
DanielaMoreiraTorres <mishellmoreira_1997@hotmail.com>
2022-01-11 15:27:20 -0500

    CREATE TOOLBAR AND ACTIVITY_NIVELES, UPDATE MENUADMIN Y WELCOME

--- app/src/main/java/com/example/tolan/MenuAdmin.java
+++ app/src/main/java/com/example/tolan/MenuAdmin.java
@@ -1,15 +1,43 @@
 package com.example.tolan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MenuAdmin extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_admin);
-        getSupportActionBar().hide();
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.btnLogIn) {
+            Intent intent = new Intent(this, Login.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        }
+        if(id == R.id.btnContacts) {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
