4d1de289c2e209af366c36d8506641ee1cf706a4
DanielaMoreiraTorres <mishellmoreira_1997@hotmail.com>
2022-02-01 00:49:54 -0500

    ACTIVITIES STATIC V1

---
+++ app/src/main/java/com/example/tolan/ActivityIdentifyImg.java
@@ -0,0 +1,14 @@
+package com.example.tolan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityIdentifyImg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identify_img);
+    }
+}
---
+++ app/src/main/java/com/example/tolan/ActivitySelectPairsImgImg.java
@@ -0,0 +1,14 @@
+package com.example.tolan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySelectPairsImgImg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_pairs_img_img);
+    }
+}
