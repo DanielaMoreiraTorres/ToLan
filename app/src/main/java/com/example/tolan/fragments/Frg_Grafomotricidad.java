ec3a297718503e6dbcaaa839cf3449c8819ab8a1
AngelValle0396 <66800307+AngelValle0396@users.noreply.github.com>
2022-03-02 15:08:37 -0500

    Rompecabeza

--- app/src/main/java/com/example/tolan/fragments/Frg_Grafomotricidad.java
+++ app/src/main/java/com/example/tolan/fragments/Frg_Grafomotricidad.java
@@ -1,17 +1,30 @@
 package com.example.tolan.fragments;
 
+import static android.content.ClipData.newPlainText;
+
+import android.content.ClipData;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.NavOptions;
 import androidx.navigation.Navigation;
 
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tolan.R;
@@ -92,6 +105,128 @@ public class Frg_Grafomotricidad extends Fragment implements View.OnClickListene
         }
 
         view.findViewById(R.id.btn_comprobar_actividades).setOnClickListener(this);
+    }
+    ArrayList<Bitmap> pieces;
+
+    void Llenar(View view) {
+        try {
+            CardView c1 = view.findViewById(R.id.car1);
+            ImageView pz1 = view.findViewById(R.id.imP1);
+            ImageView pz2 = view.findViewById(R.id.imP2);
+            ImageView pz3 = view.findViewById(R.id.imP3);
+            ImageView pz4 = view.findViewById(R.id.imP4);
+            ImageView tr1 = view.findViewById(R.id.prt1);
+            ImageView tr2 = view.findViewById(R.id.prt2);
+            ImageView tr3 = view.findViewById(R.id.prt3);
+            ImageView tr4 = view.findViewById(R.id.prt4);
+            pieces = splitImage(view);
+            pz1.setImageBitmap(pieces.get(0));
+            pz2.setImageBitmap(pieces.get(1));
+            pz3.setImageBitmap(pieces.get(2));
+            pz4.setImageBitmap(pieces.get(3));
+            tr1.setImageBitmap(pieces.get(0));
+            tr2.setImageBitmap(pieces.get(1));
+            tr3.setImageBitmap(pieces.get(2));
+            tr4.setImageBitmap(pieces.get(3));
+            pz1.setOnLongClickListener(v -> LongClickListener(v));
+            pz2.setOnLongClickListener(v -> LongClickListener(v));
+            pz3.setOnLongClickListener(v -> LongClickListener(v));
+            pz4.setOnLongClickListener(v -> LongClickListener(v));
+            tr1.setOnDragListener((v, event) -> DragListener(v, event));
+            tr2.setOnDragListener((v, event) -> DragListener(v, event));
+            tr3.setOnDragListener((v, event) -> DragListener(v, event));
+            tr4.setOnDragListener((v, event) -> DragListener(v, event));
+        } catch (Exception e) {
+        }
+    }
+
+    String respuesta="";
+    private boolean LongClickListener(View v) {
+        try {
+            ClipData data = newPlainText((CharSequence) v.getTag(), "");
+            // DragShadowBuilder myShadow = new DragShadowBuilder(v);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                //v.startDragAndDrop(data, myShadow, null, 0);
+
+                v.startDragAndDrop(data, new View.DragShadowBuilder(v){
+                    @Override
+                    public void onDrawShadow(Canvas canvas) {v.draw(canvas); }
+
+                    @Override
+                    public void onProvideShadowMetrics(Point outShadowSize, Point outShadowTouchPoint) {
+                        outShadowSize.set(v.getWidth(), v.getHeight());
+                        outShadowTouchPoint.set(v.getWidth() / 2, v.getHeight() / 2);
+                    }
+                }, null, 0);
+            }
+            else
+                v.startDrag(data, new View.DragShadowBuilder(v){
+                    @Override
+                    public void onDrawShadow(Canvas canvas) {v.draw(canvas); }
+
+                    @Override
+                    public void onProvideShadowMetrics(Point outShadowSize, Point outShadowTouchPoint) {
+                        outShadowSize.set(v.getWidth(), v.getHeight());
+                        outShadowTouchPoint.set(v.getWidth() / 2, v.getHeight() / 2);
+                    }
+                }, null, 0);
+        } catch (Exception e) {}
+        return true;
+    }
+    public boolean DragListener(View view, @NonNull DragEvent dragEvent) {
+        switch (dragEvent.getAction()) {
+            case DragEvent.ACTION_DRAG_ENDED:
+                break;
+            case DragEvent.ACTION_DRAG_STARTED:
+                break;
+            case DragEvent.ACTION_DRAG_LOCATION:
+                break;
+            case DragEvent.ACTION_DROP:
+                if(view.getTag().toString().equals(dragEvent.getClipDescription().getLabel())) {
+                    TextView n = view.findViewById(R.id.txtMenu);
+                    view.setBackgroundColor(Color.GREEN);
+                    respuesta+="1";
+                }
+                else {
+                    view.setBackgroundColor(Color.RED);
+                }
+                break;
+            case DragEvent.ACTION_DRAG_ENTERED:
+                view.invalidate();
+                break;
+            default:
+                break;
+        }
+        return true;
+    };
+    private ArrayList<Bitmap> splitImage(View v) {
+        int piecesNumber = 4;
+        int rows = 2;
+        int cols = 2;
+
+        ImageView imageView = (ImageView) v.findViewById(R.id.imgEnunciado);
+        ArrayList<Bitmap> pieces = new ArrayList<>(piecesNumber);
+
+        // Get the bitmap of the source image
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        // Calculate the with and height of the pieces
+        int pieceWidth = bitmap.getWidth()/cols;
+        int pieceHeight = bitmap.getHeight()/rows;
+
+        // Create each bitmap piece and add it to the resulting array
+        int yCoord = 0;
+        for (int row = 0; row < rows; row++) {
+            int xCoord = 0;
+            for (int col = 0; col < cols; col++) {
+                pieces.add(Bitmap.createBitmap(bitmap, xCoord, yCoord, pieceWidth, pieceHeight));
+                xCoord += pieceWidth;
+            }
+            yCoord += pieceHeight;
+        }
+
+        return pieces;
     }
 
 
